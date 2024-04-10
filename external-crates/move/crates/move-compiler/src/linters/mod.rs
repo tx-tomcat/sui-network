28253d235fb0b7725becdcd0c3bfb39f6aa4ec2e
jamedzung <dung.dinhnguyen@digitalavenues.com>
2024-04-11 02:31:39 +0700

    implement rules

--- external-crates/move/crates/move-compiler/src/linters/mod.rs
+++ external-crates/move/crates/move-compiler/src/linters/mod.rs
@@ -5,9 +5,9 @@ use move_symbol_pool::Symbol;
 
 use crate::{
     command_line::compiler::Visitor, diagnostics::codes::WarningFilter,
-    linters::shift_overflow::ShiftOperationOverflow, typing::visitor::TypingVisitor,
+    linters::almost_swapped::SwapSequence, typing::visitor::TypingVisitor,
 };
-pub mod shift_overflow;
+pub mod almost_swapped;
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum LintLevel {
     // No linters
@@ -21,11 +21,12 @@ pub enum LintLevel {
 pub const ALLOW_ATTR_CATEGORY: &str = "lint";
 pub const LINT_WARNING_PREFIX: &str = "Lint ";
 pub const SHILF_OVERFLOW_FILTER_NAME: &str = "shift_overflow";
+pub const SWAP_SEQUENCE_FILTER_NAME: &str = "swap_sequence";
 
 pub const LINTER_DEFAULT_DIAG_CODE: u8 = 1;
-
+pub const SWAP_SEQUENCE_DIAG_CODE: u8 = 8;
 pub enum LinterDiagCategory {
-    ShiftOperationOverflow,
+    Correctness,
 }
 
 pub fn known_filters() -> (Option<Symbol>, Vec<WarningFilter>) {
@@ -33,9 +34,9 @@ pub fn known_filters() -> (Option<Symbol>, Vec<WarningFilter>) {
         Some(ALLOW_ATTR_CATEGORY.into()),
         vec![WarningFilter::code(
             Some(LINT_WARNING_PREFIX),
-            LinterDiagCategory::ShiftOperationOverflow as u8,
-            LINTER_DEFAULT_DIAG_CODE,
-            Some(SHILF_OVERFLOW_FILTER_NAME),
+            LinterDiagCategory::Correctness as u8,
+            SWAP_SEQUENCE_DIAG_CODE,
+            Some(SWAP_SEQUENCE_FILTER_NAME),
         )],
     )
 }
@@ -44,9 +45,7 @@ pub fn linter_visitors(level: LintLevel) -> Vec<Visitor> {
     match level {
         LintLevel::None => vec![],
         LintLevel::Default | LintLevel::All => {
-            vec![shift_overflow::ShiftOperationOverflow::visitor(
-                ShiftOperationOverflow,
-            )]
+            vec![almost_swapped::SwapSequence::visitor(SwapSequence)]
         }
     }
 }
