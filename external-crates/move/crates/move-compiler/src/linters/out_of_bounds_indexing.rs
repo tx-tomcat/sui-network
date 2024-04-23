fdd35f4339eea014a59d6d49e1215a5edaff3b30
jamedzung <dung.dinhnguyen@digitalavenues.com>
2024-04-24 01:20:12 +0700

    refactor

--- external-crates/move/crates/move-compiler/src/linters/out_of_bounds_indexing.rs
+++ external-crates/move/crates/move-compiler/src/linters/out_of_bounds_indexing.rs
@@ -19,17 +19,17 @@ use crate::{
         visitor::{TypingVisitorConstructor, TypingVisitorContext},
     },
 };
-use move_ir_types::location::Loc;
+use move_ir_types::location::{Loc, Spanned};
 use std::collections::BTreeMap;
 
-use super::{LinterDiagCategory, LINTER_DEFAULT_DIAG_CODE, LINT_WARNING_PREFIX};
+use super::{LinterDiagCategory, LINTER_OUT_OF_BOUNDS_INDEXING_DIAG_CODE, LINT_WARNING_PREFIX};
 
-const SHIFT_OPERATION_OVERFLOW_DIAG: DiagnosticInfo = custom(
+const OUT_OF_BOUNDS_INDEXING_DIAG: DiagnosticInfo = custom(
     LINT_WARNING_PREFIX,
     Severity::Warning,
     LinterDiagCategory::Correctness as u8,
-    LINTER_DEFAULT_DIAG_CODE,
-    "Potential overflow detected. The number of bits being shifted exceeds the bit width of the variable being shifted.",
+    LINTER_OUT_OF_BOUNDS_INDEXING_DIAG_CODE,
+    "Array index out of bounds: attempting to access index {} in array '{}' with size known at compile time.",
 );
 
 pub struct OutOfBoundsArrayIndexing;
@@ -61,8 +61,9 @@ impl TypingVisitorContext for Context<'_> {
         _function_name: FunctionName,
         fdef: &mut T::Function,
     ) -> bool {
-        if let T::FunctionBody_::Defined(seq) = &mut fdef.body.value {
-            seq.1.iter().for_each(|seq_item| {
+        if let T::FunctionBody_::Defined((_, seq)) = &fdef.body.value {
+            for seq_item in seq {
+                use T::SequenceItem_ as SI;
                 if let T::SequenceItem_::Bind(value_list, _, seq_exp) = &seq_item.value {
                     if let UnannotatedExp_::Vector(_, size, _, _) = &seq_exp.exp.value {
                         if let Some(sp!(_, LValue_::Var { var, .. })) = &value_list.value.get(0) {
@@ -70,39 +71,21 @@ impl TypingVisitorContext for Context<'_> {
                         }
                     }
                 }
-            });
-        }
-        false
-    }
-    fn visit_exp_custom(&mut self, exp: &mut T::Exp) -> bool {
-        if let UnannotatedExp_::ModuleCall(module_call) = &exp.exp.value {
-            let a = is_vector_borrow(module_call);
-            if let UnannotatedExp_::ExpList(exp_list) = &module_call.arguments.exp.value {
-                if let Some(ExpListItem::Single(arr_arg_exp, _)) = &exp_list.get(0) {
-                    if let UnannotatedExp_::BorrowLocal(_, sp!(_, array_arg)) =
-                        &arr_arg_exp.exp.value
-                    {
-                        if let Some(array_size) = self.array_list.get(array_arg) {
-                            if let Some(ExpListItem::Single(value_exp, _)) = &exp_list.get(1) {
-                                if let UnannotatedExp_::Value(sp!(_, size)) = &value_exp.exp.value {
-                                    let index = extract_value(&size);
-                                    if index - 1 > *array_size as u128 {
-                                        report_out_of_bounds_indexing(
-                                            self.env,
-                                            array_arg,
-                                            index,
-                                            exp.exp.loc,
-                                        );
-                                    }
-                                }
-                            }
-                        }
+                match &seq_item.value {
+                    SI::Seq(e) => {
+                        visit_function_exp_custom(&mut self.env, &mut self.array_list, &e.exp)
                     }
-                }
+                    SI::Declare(_) => (),
+                    SI::Bind(_, _, e) => {
+                        visit_function_exp_custom(&mut self.env, &mut self.array_list, &e.exp)
+                    }
+                };
             }
         }
+        self.array_list.clear();
         false
     }
+
     fn add_warning_filter_scope(&mut self, filter: WarningFilters) {
         self.env.add_warning_filter_scope(filter)
     }
@@ -112,9 +95,72 @@ impl TypingVisitorContext for Context<'_> {
     }
 }
 
+fn visit_function_exp_custom(
+    env: &mut CompilationEnv,
+    array_list: &mut BTreeMap<Var_, usize>,
+    exp: &Spanned<UnannotatedExp_>,
+) {
+    if let UnannotatedExp_::ModuleCall(module_call) = &exp.value {
+        if is_array_push(module_call) {
+            if let UnannotatedExp_::ExpList(exp_list) = &module_call.arguments.exp.value {
+                if let Some(ExpListItem::Single(arr_arg_exp, _)) = &exp_list.get(0) {
+                    if let UnannotatedExp_::BorrowLocal(_, sp!(_, array_arg)) =
+                        &arr_arg_exp.exp.value
+                    {
+                        if let Some(array_size) = array_list.get(array_arg) {
+                            array_list.insert(*array_arg, array_size + 1);
+                        }
+                    }
+                }
+            }
+        }
+        if is_array_pop(module_call) {
+            if let UnannotatedExp_::BorrowLocal(_, sp!(_, array_arg)) =
+                &module_call.arguments.exp.value
+            {
+                if let Some(array_size) = array_list.get(array_arg) {
+                    array_list.insert(*array_arg, array_size - 1);
+                }
+            }
+        }
+        if is_vector_borrow(module_call) {
+            if let UnannotatedExp_::ExpList(exp_list) = &module_call.arguments.exp.value {
+                if let Some(ExpListItem::Single(arr_arg_exp, _)) = &exp_list.get(0) {
+                    if let UnannotatedExp_::BorrowLocal(_, sp!(_, array_arg)) =
+                        &arr_arg_exp.exp.value
+                    {
+                        if let Some(array_size) = array_list.get(array_arg) {
+                            if let Some(ExpListItem::Single(value_exp, _)) = &exp_list.get(1) {
+                                if let UnannotatedExp_::Value(sp!(_, size)) = &value_exp.exp.value {
+                                    let index = extract_value(&size);
+                                    if index > (*array_size as u128 - 1) {
+                                        report_out_of_bounds_indexing(
+                                            env, array_arg, index, exp.loc,
+                                        );
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
 fn is_vector_borrow(module_call: &ModuleCall) -> bool {
     (module_call.name.0.value.as_str() == "borrow"
         || module_call.name.0.value.as_str() == "borrow_mut")
+        && module_call.module.value.module.0.value.as_str() == "vector"
+}
+
+fn is_array_push(module_call: &ModuleCall) -> bool {
+    module_call.name.0.value.as_str() == "push_back"
+        && module_call.module.value.module.0.value.as_str() == "vector"
+}
+
+fn is_array_pop(module_call: &ModuleCall) -> bool {
+    module_call.name.0.value.as_str() == "pop_back"
         && module_call.module.value.module.0.value.as_str() == "vector"
 }
 
@@ -133,6 +179,6 @@ fn report_out_of_bounds_indexing(env: &mut CompilationEnv, var: &Var_, index: u1
         "Array index out of bounds: attempting to access index {} in array '{}' with size known at compile time.",
         index, var.name.as_str()
     );
-    let diag = diag!(SHIFT_OPERATION_OVERFLOW_DIAG, (loc, msg));
+    let diag = diag!(OUT_OF_BOUNDS_INDEXING_DIAG, (loc, msg));
     env.add_diag(diag);
 }
