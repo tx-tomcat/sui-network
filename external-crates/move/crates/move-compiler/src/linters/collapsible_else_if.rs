6e9d07d2622fd31c1609710e4d3f22c2151a6acf
dzungdinh94 <dungnd8594@gmail.com>
2024-05-14 17:50:44 +0700

    Update collapsible_else_if.rs

--- external-crates/move/crates/move-compiler/src/linters/collapsible_else_if.rs
+++ external-crates/move/crates/move-compiler/src/linters/collapsible_else_if.rs
@@ -15,13 +15,13 @@ use crate::{
 };
 use move_ir_types::location::Loc;
 
-use super::{LinterDiagCategory, LINTER_DEFAULT_DIAG_CODE, LINT_WARNING_PREFIX};
+use super::{LinterDiagnosticCategory, COLLAPSIBLE_ELSE_DIAG_CODE, LINT_WARNING_PREFIX};
 
 const COLLAPSIBLE_ELSE_DIAG: DiagnosticInfo = custom(
     LINT_WARNING_PREFIX,
     Severity::Warning,
-    LinterDiagCategory::CollapsibleElseIf as u8,
-    LINTER_DEFAULT_DIAG_CODE,
+    LinterDiagnosticCategory::Style as u8,
+    COLLAPSIBLE_ELSE_DIAG_CODE,
     "",
 );
 
