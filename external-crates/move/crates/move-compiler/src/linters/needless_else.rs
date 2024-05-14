d0975cc772a80b1449e5037b5ba6859c7ab8f8c0
dzungdinh94 <dungnd8594@gmail.com>
2024-05-14 16:55:39 +0700

    Update needless_else.rs

--- external-crates/move/crates/move-compiler/src/linters/needless_else.rs
+++ external-crates/move/crates/move-compiler/src/linters/needless_else.rs
@@ -15,13 +15,13 @@ use crate::{
 };
 use move_ir_types::location::Loc;
 
-use super::{LinterDiagCategory, LINTER_DEFAULT_DIAG_CODE, LINT_WARNING_PREFIX};
+use super::{LinterDiagCategory, EMPTY_ELSE_BRANCH_DIAG_CODE, LINT_WARNING_PREFIX};
 
 const EMPTY_ELSE_BRANCH_DIAG: DiagnosticInfo = custom(
     LINT_WARNING_PREFIX,
     Severity::Warning,
-    LinterDiagCategory::EmptyElseBranch as u8,
-    LINTER_DEFAULT_DIAG_CODE,
+    LinterDiagCategory::Complexity as u8,
+    EMPTY_ELSE_BRANCH_DIAG_CODE,
     "",
 );
 
