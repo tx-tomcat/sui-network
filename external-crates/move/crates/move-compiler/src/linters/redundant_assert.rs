9a3f57498893e1293c67b1ab0f123cce62bfb4b7
dzungdinh94 <dungnd8594@gmail.com>
2024-05-14 17:18:34 +0700

    Update redundant_assert.rs

--- external-crates/move/crates/move-compiler/src/linters/redundant_assert.rs
+++ external-crates/move/crates/move-compiler/src/linters/redundant_assert.rs
@@ -16,13 +16,13 @@ use crate::{
 };
 use move_ir_types::location::Loc;
 
-use super::{LinterDiagCategory, LINTER_DEFAULT_DIAG_CODE, LINT_WARNING_PREFIX};
+use super::{LinterDiagCategory, LINT_WARNING_PREFIX, REDUNDANT_ASSERT_DIAG_CODE};
 
 const REDUNDANT_ASSERT_DIAG: DiagnosticInfo = custom(
     LINT_WARNING_PREFIX,
     Severity::Warning,
-    LinterDiagCategory::AssertTrueFals as u8,
-    LINTER_DEFAULT_DIAG_CODE,
+    LinterDiagCategory::Correctness as u8,
+    REDUNDANT_ASSERT_DIAG_CODE,
     "",
 );
 
