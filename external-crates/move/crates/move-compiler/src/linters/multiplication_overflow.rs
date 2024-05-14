ef7226f46875fa4456edfcb526337475331fd43c
dzungdinh94 <dungnd8594@gmail.com>
2024-05-14 17:25:39 +0700

    Update multiplication_overflow.rs

--- external-crates/move/crates/move-compiler/src/linters/multiplication_overflow.rs
+++ external-crates/move/crates/move-compiler/src/linters/multiplication_overflow.rs
@@ -18,13 +18,13 @@ use crate::{
 use move_ir_types::location::Loc;
 use std::str::FromStr;
 
-use super::{LinterDiagCategory, LINTER_DEFAULT_DIAG_CODE, LINT_WARNING_PREFIX};
+use super::{LinterDiagCategory, LINT_WARNING_PREFIX, MULTIPLICATION_OVERFLOW_DIAG_CODE};
 
 const SHIFT_OPERATION_OVERFLOW_DIAG: DiagnosticInfo = custom(
     LINT_WARNING_PREFIX,
     Severity::Warning,
-    LinterDiagCategory::MultiplicationOverflow as u8,
-    LINTER_DEFAULT_DIAG_CODE,
+    LinterDiagCategory::Suspicious as u8,
+    MULTIPLICATION_OVERFLOW_DIAG_CODE,
     "Potential overflow detected in multiplication operation",
 );
 
